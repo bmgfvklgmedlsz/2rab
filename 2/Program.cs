cce4771ad819e47a3f212e4850f4f786c0dace8a
Blik-5 <svet26102001@gmail.com>
2025-03-24 11:07:12 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(2, 3);
+
+
+            rect.ShowData();
+
+
+            double perimeter = rect.Perimetr();
+            Console.WriteLine($"Периметр: {perimeter}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2/Rectangle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    public class Rectangle
+    {
+
+        private double height;
+        private double width;
+
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Высота должна быть положительной.");
+                    height = 1;
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Ширина должна быть положительной.");
+                    width = 1;
+                }
+                else
+                {
+                    width = value;
+                }
+            }
+        }
+
+
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public Rectangle() : this(1, 1) { }
+
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Высота: {Height}, Ширина: {Width}");
+        }
+
+        public double Perimetr()
+        {
+            return 2 * (Height + Width);
+        }
+    }
+}
